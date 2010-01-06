68e484d1ecb5bd63098769815d5e4ebe3e9af5d9
Jason Whitlark <jw@bohr.(none)>
2010-01-05 23:49:00 -0800

    rocking...

--- p2pvpnmgr/NetworkdetailDialog.py
+++ p2pvpnmgr/NetworkdetailDialog.py
@@ -11,6 +11,8 @@ from desktopcouch.records.record import Record
 
 from p2pvpnmgr.p2pvpnmgrconfig import getdatapath
 
+record_type = "http://unum.whitlark.org/todo/p2pvpnDoc"
+
 class NetworkdetailDialog(gtk.Dialog):
     __gtype_name__ = "NetworkdetailDialog"
 
@@ -37,13 +39,27 @@ class NetworkdetailDialog(gtk.Dialog):
         self.builder.connect_signals(self)
 
 
+    def load_from_couch(self, _id):
+
+        target = self.database.get_record(_id)
+
+        if target:
+            self.builder.get_object("net_name").set_text(target["net_name"])
+            self.builder.get_object("ifc_name").set_text(target["ifc_name"])
+            self.builder.get_object("supernode_address").set_text(target["supernode_address"])
+            self.builder.get_object("log_location").set_text(target["log_location"])
+            self.builder.get_object("autostart").set_active(target["autostart"])
+            self.builder.get_object("allow_broadcast").set_active(target["allow_broadcast"])
+            self.builder.get_object("db_replicate").set_text(target["db_replicate"])
+
     def ok(self, widget, data=None):
         """ok - The user has elected to save the changes.
         Called before the dialog returns gtk.RESONSE_OK from run().
 
         """
-        record_type = "http://unum.whitlark.org/todo/p2pvpnDoc"
         net_name = self.builder.get_object("net_name").get_text()  #or set_text
+
+
         ifc_name = self.builder.get_object("ifc_name").get_text()
         supernode_address = self.builder.get_object("supernode_address").get_text()
         log_location = self.builder.get_object("log_location").get_text()
@@ -51,10 +67,22 @@ class NetworkdetailDialog(gtk.Dialog):
         allow_broadcast = self.builder.get_object("allow_broadcast").get_active()
         db_replicate = self.builder.get_object("db_replicate").get_text()
 
-        print "Save called: %s, %s, %s, %s" % (net_name, ifc_name, supernode_address, log_location)
+        print "Save called: %s, %s, %s, %s, %s" % (net_name, ifc_name, supernode_address, log_location, allow_broadcast)
 
         results = self.database.get_records(record_type = record_type,
                                             create_view = True)
+
+        #update a record that has the same title
+        for result in results:
+            document = result.value
+            if document["net_name"] == net_name:
+                self.database.update_fields(result.id, {"ifc_name": ifc_name,
+                                                  "supernode_address": supernode_address,
+                                                  "log_location": log_location,
+                                                  "db_replicate": db_replicate,
+                                                  "autostart": autostart,
+                                                  "allow_broadcast": allow_broadcast,})
+                return
 
         new_rec = Record({"record_type": record_type,
                           "net_name": net_name,
@@ -63,9 +91,8 @@ class NetworkdetailDialog(gtk.Dialog):
                           "log_location": log_location,
                           "db_replicate": db_replicate,
                           "autostart": autostart,
-                          "allows_broadcast": allow_broadcast,})
+                          "allow_broadcast": allow_broadcast,})
         self.database.put_record(new_rec)
-        self.destroy()
 
 
     def cancel(self, widget, data=None):
@@ -73,9 +100,9 @@ class NetworkdetailDialog(gtk.Dialog):
         Called before the dialog returns gtk.RESPONSE_CANCEL for run()
 
         """
-        self.destroy()
+        pass
 
-def NewNetworkdetailDialog(database):
+def NewNetworkdetailDialog(database, _id=None):
     """NewNetworkdetailDialog - returns a fully instantiated
     dialog-camel_case_nameDialog object. Use this function rather than
     creating NetworkdetailDialog instance directly.
@@ -92,6 +119,8 @@ def NewNetworkdetailDialog(database):
     dialog = builder.get_object("networkdetail_dialog")
     dialog.finish_initializing(builder)
     dialog.database = database
+    if _id is not None:
+        dialog.load_from_couch(_id)
     return dialog
 
 if __name__ == "__main__":
